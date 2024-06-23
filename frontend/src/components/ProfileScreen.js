498d8d669bd71701f72eb78ca4caff878c7770be
Christos Hadjigeorghiou <christoshadjigeorghiou@gmail.com>
2024-06-23 14:05:12 +0200

    Add head of medusa svg and update readme

--- frontend/src/components/ProfileScreen.js
+++ frontend/src/components/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './ProfileScreen.css'
+import { ReactComponent as HeadOfMedusaIcon } from './../assets/head_of_medusa.svg';
 
 const ProfileScreen = ({ show, onClose, items }) => {
   if (!show) return null;
@@ -21,6 +22,11 @@ const ProfileScreen = ({ show, onClose, items }) => {
                 <h4>{item.name}</h4>
                 <p>{item.description}</p>
                 <p><em>{item.obtained ? 'Equipped' : `Obtain by: ${item.obtainCondition}`}</em></p>
+                <div className="item-icon">
+                  {item.name === "Head of Medusa" && (
+                    <HeadOfMedusaIcon />
+                  )}
+                </div>
               </div>
             ))}
           </div>
